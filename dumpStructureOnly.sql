5a626a16c0ae36a57beee5f4edb77e166dec572a
Dazannah <beattius@gmail.com>
2025-03-07 21:05:18 +0100

    dumpStructure added

---
+++ dumpStructureOnly.sql
@@ -0,0 +1,135 @@
+CREATE DATABASE  IF NOT EXISTS `bakery` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bakery`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 192.168.1.222    Database: bakery
+-- ------------------------------------------------------
+-- Server version	8.0.41-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Ingredients`
+--
+
+DROP TABLE IF EXISTS `Ingredients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Ingredients` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Recipes`
+--
+
+DROP TABLE IF EXISTS `Recipes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Recipes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `lactoseFree` tinyint NOT NULL,
+  `glutenFree` tinyint NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `RecipesIngredients`
+--
+
+DROP TABLE IF EXISTS `RecipesIngredients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `RecipesIngredients` (
+  `recipeId` int NOT NULL,
+  `ingredientId` int NOT NULL,
+  `amount` int NOT NULL,
+  `unitId` int NOT NULL,
+  KEY `RecipeId_idx` (`recipeId`),
+  KEY `IngredientId_idx` (`ingredientId`),
+  KEY `UnitId_idx` (`unitId`),
+  CONSTRAINT `IngredientId` FOREIGN KEY (`ingredientId`) REFERENCES `Ingredients` (`id`),
+  CONSTRAINT `RecipeId` FOREIGN KEY (`recipeId`) REFERENCES `Recipes` (`id`),
+  CONSTRAINT `UnitId` FOREIGN KEY (`unitId`) REFERENCES `Units` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `SalesOfLastWeek`
+--
+
+DROP TABLE IF EXISTS `SalesOfLastWeek`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `SalesOfLastWeek` (
+  `id` int NOT NULL,
+  `recipeId` int NOT NULL,
+  `amount` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `recipeId_UNIQUE` (`recipeId`),
+  KEY `recipeId_idx` (`recipeId`),
+  CONSTRAINT `recipeIdFK` FOREIGN KEY (`recipeId`) REFERENCES `Recipes` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Units`
+--
+
+DROP TABLE IF EXISTS `Units`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Units` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `WholesalePrice`
+--
+
+DROP TABLE IF EXISTS `WholesalePrice`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `WholesalePrice` (
+  `id` int NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `amount` int NOT NULL,
+  `unitId` int NOT NULL,
+  `price` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`),
+  KEY `unitId_idx` (`unitId`),
+  CONSTRAINT `unitIdFK` FOREIGN KEY (`unitId`) REFERENCES `Units` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-03-07 21:01:50
