75aca8ecdd703dff623e0292bcfa0b7e3ca0dbaa
Dazannah <beattius@gmail.com>
2025-03-07 21:59:50 +0100

    add units to database

--- services/startingDataService.php
+++ services/startingDataService.php
@@ -1,6 +1,7 @@
 <?php
 require_once "./singletons/db.php";
 require_once "./models/Ingredient.php";
+require_once "./models/Unit.php";
 
 class StartingDataService {
   public static function addProvidedData() {
@@ -15,6 +16,22 @@ class StartingDataService {
     foreach ($data->inventory as $inventory) {
       $ingredient = new Ingredient($inventory->name);
       $sql = $ingredient->getCreateSql();
+
+      try {
+        $db->query($sql);
+      } catch (Exception $ex) {
+        if (!preg_match($duplicateRegexp, $ex->getMessage()))
+          echo $ex->getMessage() . "<br/>";
+      }
+    }
+
+    $dataLocation = "units.json";
+    $dataFile = fopen($dataLocation, "r") or die("Unable to open units file!");
+    $data = json_decode(fread($dataFile, filesize($dataLocation)));
+
+    foreach ($data as $unit) {
+      $unit = new Unit($unit);
+      $sql = $unit->getCreateSql();
 
       try {
         $db->query($sql);
