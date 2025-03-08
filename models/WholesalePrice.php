782419a25485e42aa296e2fdec9337de8141be00
Dazannah <beattius@gmail.com>
2025-03-08 08:47:24 +0100

    added WholesalePrice

---
+++ models/WholesalePrice.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once "./interfaces/IModel.php";
+
+class WholesalePrice implements IModel {
+  public int|null $id;
+  public int $ingredientId;
+  public int $amount;
+  public int $unitId;
+  public int $price;
+
+  public function __construct(int $ingredientId, int $amount, int $unitID, int $price, int|null $id = null) {
+    $this->id = $id;
+    $this->ingredientId = $ingredientId;
+    $this->amount = $amount;
+    $this->unitId = $unitID;
+    $this->price = $price;
+  }
+
+  public function getCreateSql(): string {
+    return "INSERT INTO WholesalePrice(ingredientId, amount, unitId, price) VALUES('$this->ingredientId','$this->amount','$this->unitId','$this->price')";
+  }
+}
