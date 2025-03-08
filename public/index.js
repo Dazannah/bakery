d80b8129200ceb8eed2d923a9c7e01778d09a119
Dazannah <beattius@gmail.com>
2025-03-08 13:35:20 +0100

    fetch /three-column added

--- public/index.js
+++ public/index.js
@@ -25,3 +25,35 @@ fetch("/last-week-profit")
     lastWeekIncome.innerText = data;
   })
   .catch(error => console.log(error));
+
+fetch("/three-column")
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      console.log(response.statusText);
+    }
+  })
+  .then(data => {
+    const glutenFreeTable = document.getElementById("gluten-free").getElementsByTagName("tbody")[0];
+    const lactoseFreeTable = document.getElementById("lactose-free").getElementsByTagName("tbody")[0];
+    const glutenAndLactoseFree = document.getElementById("gluten-and-lactose-free").getElementsByTagName("tbody")[0];
+
+    glutenFreeTable.innerHTML = generateTbody(data.glutenFree);
+    lactoseFreeTable.innerHTML = generateTbody(data.lactoseFree);
+    glutenAndLactoseFree.innerHTML = generateTbody(data.glutenAndLactoseFree);
+  })
+  .catch(error => console.log(error));
+
+function generateTbody(array) {
+  let resultHtml = "";
+  array.forEach(food => {
+    resultHtml += `<tr>`;
+    food.forEach(element => {
+      resultHtml += `<td>${element}${isNaN(element) ? "" : " Ft"}</td>`;
+    });
+    resultHtml += `</tr>`;
+  });
+
+  return resultHtml;
+}
