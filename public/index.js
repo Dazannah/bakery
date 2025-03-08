3b99333a75a2613ad6c36ebb6e1bdcf8f4ca7dc6
Dazannah <beattius@gmail.com>
2025-03-08 11:19:39 +0100

    last-week-profit fetch created

--- public/index.js
+++ public/index.js
@@ -11,3 +11,17 @@ fetch("/last-week-income")
     lastWeekIncome.innerText = data;
   })
   .catch(error => console.log(error));
+
+fetch("/last-week-profit")
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      console.log(response.statusText);
+    }
+  })
+  .then(data => {
+    const lastWeekIncome = document.getElementById("last-week-profit");
+    lastWeekIncome.innerText = data;
+  })
+  .catch(error => console.log(error));
