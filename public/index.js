640699ae71b445c3656d401908b3e1b41cea8431
Dazannah <beattius@gmail.com>
2025-03-08 10:41:23 +0100

    index.js created

---
+++ public/index.js
@@ -0,0 +1,12 @@
+fetch("/last-week-income")
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      console.log(response.statusText);
+    }
+  })
+  .then(data => {
+    const lastWeekIncome = document.getElementById("last-week-income");
+    lastWeekIncome.innerText = data;
+  });
