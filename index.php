74a109c7fbeebb1e1d85561c1d497ad4a65e9030
Dazannah <beattius@gmail.com>
2025-03-08 08:49:57 +0100

    use StartingDataService

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 require_once "./singletons/router.php";
+require_once "./services/startingDataService.php";
+
+StartingDataService::addProvidedData();
+
 $router = Router::getInstance();
+
+// $router->addRoute("/test", function () {
+//   echo "this is from the function";
+// });
 
 $router->use();
