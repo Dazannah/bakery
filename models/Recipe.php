983df93994994d6f1398bf068182411fbdfb0de3
Dazannah <beattius@gmail.com>
2025-03-08 08:11:36 +0100

    recipe constructor and getCreateSql added

--- models/Recipe.php
+++ models/Recipe.php
@@ -3,8 +3,22 @@ class Recipe {
   public int $id;
   public string $name;
   public int $price;
-  public bool $lactoseFree;
-  public bool $glutenFree;
-  /** @var Ingredients[] */
-  public array $ingredients;
+  public int $lactoseFree;
+  public int $glutenFree;
+  /** @var array{ingredientId: int, unitId: int, ammount: int} */
+  public array $ingredientIds;
+
+  public function __construct(string $name, string $price, bool $lactoseFree, bool $glutenFree, int|null $id = null) {
+    if ($id)
+      $this->id = $id;
+    $this->name = $name;
+    $this->price = +explode(" ", $price)[0];
+    $this->lactoseFree = $lactoseFree;
+    $this->glutenFree = $glutenFree;
+    //$ingredients;
+  }
+
+  public function getCreateSql() {
+    return "INSERT INTO Recipes(name,price,lactoseFree,glutenFree) VALUES('$this->name','$this->price','$this->lactoseFree','$this->glutenFree')";
+  }
 }
