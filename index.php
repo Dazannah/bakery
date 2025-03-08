0e95eca21556d826c8fd3cfdc9c67b8c9accd148
Dazannah <beattius@gmail.com>
2025-03-08 11:19:04 +0100

    /last-week-profit route added

--- index.php
+++ index.php
@@ -3,12 +3,14 @@ require_once "./singletons/router.php";
 require_once "./services/startingDataService.php";
 
 require_once "./controllers/IncomeController.php";
+require_once "./controllers/ProfitController.php";
 
 StartingDataService::addProvidedData();
 
 $router = Router::getInstance();
 
 $router->addRoute("/last-week-income", [IncomeController::class, "lastWeekIncome"]);
+$router->addRoute("/last-week-profit", [ProfitController::class, "lastWeekProfit"]);
 
 Router::setCatchAllRoute("/404");
 $router->addRoute("/404", function () {
