0b448d06c9832dca1da5acbea3a9b858a93ecec5
Dazannah <beattius@gmail.com>
2025-03-09 22:08:55 +0100

    InventoryController created

---
+++ controllers/InventoryController.php
@@ -0,0 +1,18 @@
+<?php
+require_once "./interfaces/IInventoryService.php";
+require_once "./services/inventoryService.php";
+
+class InventoryController {
+  private IInventoryService $inventoryService;
+
+  public function __construct() {
+    $this->inventoryService = new InventoryService(Database::getInstance(), new RecipeService(Database::getInstance(), new IngredientService(Database::getInstance())));
+  }
+
+  public function getMaxProducableFromInventory() {
+    $maxProducable = $this->inventoryService->getMaxProducableFromInventory();
+    echo json_encode($maxProducable);
+
+    exit;
+  }
+}
