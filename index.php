c5a0c3735e1d6a448afb63a871ade7c2a687f329
Dazannah <beattius@gmail.com>
2025-03-09 22:08:42 +0100

    /max-producable route added

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once "./services/startingDataService.php";
 require_once "./controllers/IncomeController.php";
 require_once "./controllers/ProfitController.php";
 require_once "./controllers/RecipeController.php";
+require_once "./controllers/InventoryController.php";
 
 StartingDataService::addProvidedData();
 
@@ -13,6 +14,7 @@ $router = Router::getInstance();
 $router->addRoute("/last-week-income", [IncomeController::class, "lastWeekIncome"]);
 $router->addRoute("/last-week-profit", [ProfitController::class, "lastWeekProfit"]);
 $router->addRoute("/three-column", [RecipeController::class, "threeColumn"]);
+$router->addRoute("/max-producable", [InventoryController::class, "getMaxProducableFromInventory"]);
 
 Router::setCatchAllRoute("/404");
 $router->addRoute("/404", function () {
