2f51e2d38a10becc3c2869d02d3f108ca35c3252
Dazannah <beattius@gmail.com>
2025-03-09 20:04:37 +0100

    Recipe contructor made to not process string

--- models/Recipe.php
+++ models/Recipe.php
@@ -7,16 +7,13 @@ class Recipe implements IModel {
   public int $price;
   public int $lactoseFree;
   public int $glutenFree;
-  /** @var array{ingredientId: int, unitId: int, ammount: int} */
-  public array $ingredientIds;
 
-  public function __construct(string $name, string $price, bool $lactoseFree, bool $glutenFree, int|null $id = null) {
+  public function __construct(string $name, int $price, bool $lactoseFree, bool $glutenFree, int|null $id = null) {
     $this->id = $id;
     $this->name = $name;
-    $this->price = +explode(" ", $price)[0];
+    $this->price = $price;
     $this->lactoseFree = $lactoseFree;
     $this->glutenFree = $glutenFree;
-    //$ingredients;
   }
 
   public function getCreateSql(): string {
