906a402c356cb49a3579397d3573297c224bd943
Dazannah <beattius@gmail.com>
2025-03-09 20:02:59 +0100

    added dependency injection

--- controllers/ProfitController.php
+++ controllers/ProfitController.php
@@ -1,13 +1,19 @@
 <?php
 require_once "./singletons/db.php";
 require_once "./services/profitService.php";
-require_once "./interfaces/IProfitService.php";
+require_once "./services/profitService.php";
+require_once "./services/wholesalePriceService.php";
 
 class ProfitController {
   private IProfitService $profitService;
 
   public function __construct() {
-    $this->profitService = new ProfitService(Database::getInstance(), new IncomeService(Database::getInstance()));
+    $this->profitService = new ProfitService(
+      Database::getInstance(),
+      new IncomeService(Database::getInstance()),
+      new WholesalePriceService(Database::getInstance()),
+      new RecipeService(Database::getInstance())
+    );
   }
 
   public function lastWeekProfit(): void {
