0887e81599a3417c9bdee67fede8bdf83200d293
Dazannah <beattius@gmail.com>
2025-03-09 22:09:52 +0100

    fetch /max-producable added

--- public/index.js
+++ public/index.js
@@ -21,7 +21,6 @@ fetch("/last-week-profit")
     }
   })
   .then(data => {
-    console.log(data);
     const lastWeekProfit = document.getElementById("last-week-profit");
     lastWeekProfit.innerText = data;
   })
@@ -40,18 +39,33 @@ fetch("/three-column")
     const lactoseFreeTable = document.getElementById("lactose-free").getElementsByTagName("tbody")[0];
     const glutenAndLactoseFree = document.getElementById("gluten-and-lactose-free").getElementsByTagName("tbody")[0];
 
-    glutenFreeTable.innerHTML = generateTbody(data.glutenFree);
-    lactoseFreeTable.innerHTML = generateTbody(data.lactoseFree);
-    glutenAndLactoseFree.innerHTML = generateTbody(data.glutenAndLactoseFree);
+    glutenFreeTable.innerHTML = generateTbody(data.glutenFree, "Ft");
+    lactoseFreeTable.innerHTML = generateTbody(data.lactoseFree, "Ft");
+    glutenAndLactoseFree.innerHTML = generateTbody(data.glutenAndLactoseFree, "Ft");
   })
   .catch(error => console.log(error));
 
-function generateTbody(array) {
+fetch("/max-producable")
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      console.log(response.statusText);
+    }
+  })
+  .then(data => {
+    console.log(data);
+    const maxProducable = document.getElementById("max-producable").getElementsByTagName("tbody")[0];
+
+    maxProducable.innerHTML = generateTbody(data, "db");
+  });
+
+function generateTbody(array, unit) {
   let resultHtml = "";
   array.forEach(food => {
     resultHtml += `<tr>`;
     food.forEach(element => {
-      resultHtml += `<td>${element}${isNaN(element) ? "" : " Ft"}</td>`;
+      resultHtml += `<td>${element}${isNaN(element) ? "" : ` ${unit}`}</td>`;
     });
     resultHtml += `</tr>`;
   });
