5794c1553f5d8d0840fc05cb75302ec3557680e4
Dazannah <beattius@gmail.com>
2025-03-10 19:56:45 +0100

    inventory, adn unitId added

--- models/Ingredient.php
+++ models/Ingredient.php
@@ -5,13 +5,17 @@ require_once "./interfaces/IModel.php";
 class Ingredient implements IModel {
   public int|null $id;
   public string $name;
+  public float $inventory;
+  public int $unitId;
 
-  public function __construct(string $name, int|null $id = null) {
+  public function __construct(string $name, float $inventory, int $unitId, int|null $id = null) {
     $this->id = $id;
     $this->name = $name;
+    $this->inventory = $inventory;
+    $this->unitId = $unitId;
   }
 
   public function getCreateSql(): string {
-    return "INSERT INTO Ingredients(name) VALUES('$this->name')";
+    return "INSERT INTO Ingredients(name, inventory, unitId) VALUES('$this->name','$this->inventory','$this->unitId')";
   }
 }
